a5bfac4b6a09e3af1b598462d25085063c85ef52
Denis <Limwaer157@gmail.com>
2023-03-25 09:10:33 +0300

    сумма рекурсия

--- Home_works/Seminar5/Task_2.py
+++ Home_works/Seminar5/Task_2.py
@@ -6,3 +6,10 @@
 #     <function_name>(0,2) -> 2
 #     <function_name>(3,0) -> 3
 
+
+def recus_summ(a,b):
+    if not (a and b):
+        return a if a else b
+    return recus_summ(a, b - 1) + 1
+
+print(recus_summ(3, 0))
